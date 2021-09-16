2d3402d000f157a24aba4a129bdcdfb4edda049a
RichardKT88 <kendy_t88@hotmail.com>
2021-09-16 16:40:21 -0300

    Chore: finaliza o componente tarefa

---
+++ src/screens/TaskList.js
@@ -0,0 +1,91 @@
+import React, { Component } from 'react'
+import { ImageBackground, Text, View, StyleSheet, FlatList } from 'react-native'
+
+import commonStyles from '../commonStyles.js'
+import todayImage from '../../assets/imgs/today.jpg'
+
+import moment from 'moment'
+import 'moment/locale/pt-br'
+
+import Task from '../components/Task.js'
+
+export default class TaskList extends Component {
+    state = {
+        tasks: [{
+            id: Math.random(),
+            desc: 'Çomprar Livro de React Native',
+            estimateAt: new Date(),
+            doneAt: new Date(),
+        },{
+            id: Math.random(),
+            desc: 'Ler Livro de React Native',
+            estimateAt: new Date(),
+            doneAt: null,
+        }]
+    }
+
+    toogleTask = taskId => {
+        const tasks = [...this.state.tasks]
+        tasks.forEach(task => {
+            if (task.id === taskId) {
+                task.doneAt = task.doneAt ? null : new Date()
+            }
+        })
+
+        this.setState({ tasks })
+    }
+
+
+    render() {
+        const today = moment().locale('pt-br').format('ddd, D [de] MMMM')
+        return (
+            <View style={styles.container}>
+                <ImageBackground source={todayImage}
+                    style={styles.background}>
+                    <View style={styles.titleBar}>
+                        <Text style={styles.title}>Hoje</Text>
+                        <Text style={styles.subtitle}>{today}</Text>
+                    </View>
+                </ImageBackground>
+                <View style={styles.taskList}>
+                    <FlatList 
+                        data={this.state.tasks}
+                        keyExtractor={item => `${item.id}`}
+                        renderItem={({item}) => <Task {...item} toogleTask={this.toogleTask} />}
+                    />            
+                </View>
+            </View>
+
+        )
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1
+    },
+    background: {
+        flex: 3
+    },
+    taskList: {
+        flex: 7
+    },
+    titleBar: {
+        flex: 1,
+        justifyContent: 'flex-end'
+    },
+    title: {
+        fontFamily: commonStyles.fontFamily,
+        color: commonStyles.colors.secondary,
+        fontSize: 50,
+        marginLeft: 20,
+        marginBottom: 20
+    },
+    subtitle: {
+        fontFamily: commonStyles.fontFamily,
+        color: commonStyles.colors.secondary,
+        fontSize: 20,
+        marginLeft: 20,
+        marginBottom: 30
+    }
+});
